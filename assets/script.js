27227307129c8412d2c726bd903d7264d70491a7
James Taylor <r92azrfn@students.codeinstitute.net>
2024-10-20 11:12:43 +0000

    added CSS to the divs to look user friendly, added some game info to be recorded when function is created and in js script, added a grid that also creates clickable divs for users to select

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,42 @@
-// set event listener for all elements on game-board, launches torpedo when square is clicked
-gameBoardContainer.addEventListener("click", launchTorpedo, false);
+// set grid rows and columns and the size of each square
+var rows = 10;
+var cols = 10;
+var squareSize = 50;
 
-// Set the grid size to put the "ships" and sizes of the squares
+// get the container element
+var gameBoardContainer = document.getElementById("game-board");
 
+// make the grid columns and rows
+for (i = 0; i < cols; i++) {
+	for (j = 0; j < rows; j++) {
+		
+		// create a new div HTML element for each grid square and make it the right size
+		var square = document.createElement("div");
+		gameBoardContainer.appendChild(square);
 
-
-// Records hits and missing within this variable
-let gameBoard = ""
+    // give each div element a unique id based on its row and column, like "s00"
+		square.id = 's' + j + i;			
+		
+		// set each grid square's coordinates: multiples of the current row or column number
+		var topPosition = j * squareSize;
+		var leftPosition = i * squareSize;			
+		
+		// use CSS absolute positioning to place each grid square on the page
+		square.style.top = topPosition + 'px';
+		square.style.left = leftPosition + 'px';						
+	}
+}
 
 // Create a score system to show when youve won
 var totalHits = "0"
+
+function gamesWon() {
+    if (totalHits = "17") {
+        alert('You have destroyed all battleships')
+	    let score = parseInt(document.getElementById("games-won").innerText);
+	    document.getElementById("games-won").innerText = ++score;
+    } 
+}
 
 /* Simple way to know if you have won, is to add up the total amount of hits to destroy all ships on the board
 and then once that number is reached the game ends. (i would like to change this in the future and have it show when
@@ -20,12 +47,18 @@ a certain ship type has been destroyed.)
       Submarine        - 3 hits
       Frigate          - 2 hits
 */
+var gameBoard = [
+				[0,0,0,1,1,1,1,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,1,0,0,0],
+				[0,0,0,0,0,0,1,0,0,0],
+				[1,0,0,0,0,0,1,1,1,1],
+				[1,0,0,0,0,0,0,0,0,0],
+				[1,0,0,1,0,0,0,0,0,0],
+				[1,0,0,1,0,0,0,0,0,0],
+				[1,0,0,0,0,0,0,0,0,0]
+				]
 
-// Randomises Ship locations on the gameboard
-function createRandomShips() {
-
-}
-
-function launchTorpedo() {
-
-}
+// set event listener for all elements in gameboard, run fireTorpedo function when square is clicked
+gameBoardContainer.addEventListener("click", fireTorpedo, false);
